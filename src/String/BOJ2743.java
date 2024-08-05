fd40c229e4e381e6546fd15b15cbe6856165aca8
gyuhyuk <bruno3@naver.com>
2024-08-05 23:11:59 +0900

    BOJ2743 : 단어 길이 재기

---
+++ src/String/BOJ2743.java
@@ -0,0 +1,11 @@
+package String;
+
+import java.util.Scanner;
+
+public class BOJ2743 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println(s.length());
+    }
+}
