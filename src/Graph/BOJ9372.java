1d2943301342287fb6d32348bf1b2a244dceff9e
gyuhyuk <bruno3@naver.com>
2025-01-10 20:08:36 +0900

    BOJ9372 : 상근이의 여행

---
+++ src/Graph/BOJ9372.java
@@ -0,0 +1,22 @@
+package Graph;
+
+import java.util.Scanner;
+
+public class BOJ9372 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int T = scanner.nextInt();
+
+        for(int i=0; i<T; i++) {
+            int N = scanner.nextInt();
+            int M = scanner.nextInt();
+
+            for(int j=0; j<M; j++) {
+                int a = scanner.nextInt();
+                int b = scanner.nextInt();
+            }
+            System.out.println(N-1);
+        }
+    }
+}
