0b0846ca3c9e2bd86b00af8981b760b078ff060a
gyuhyuk <bruno3@naver.com>
2025-01-17 23:35:10 +0900

    leetcode 121. Best Time to Buy and Sell Stock

---
+++ src/Greedy/LEETCODE121.java
@@ -0,0 +1,23 @@
+package Greedy;
+
+public class LEETCODE121 {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int min = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int price : prices) {
+            if(price < min) {
+                min = price;
+            }
+            else if(price - min > maxProfit) {
+                maxProfit  = price - min;
+            }
+        }
+
+        return maxProfit;
+    }
+}
