d908c3191327accfb731ce6f4f45b1e32c1f760c
gyuhyuk <bruno3@naver.com>
2025-01-30 18:57:57 +0900

    BOJ2748 : 알고리즘 수업 - 피보나치 수 1

--- src/DynamicProgramming/BOJ2748.java
+++ src/DynamicProgramming/BOJ2748.java
@@ -6,6 +6,8 @@ public class BOJ2748 {
     static long[] fib;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
         int n = sc.nextInt();
         fib = new long[91];
         fib[0] = 0;
@@ -13,8 +15,12 @@ public class BOJ2748 {
 
         for(int i=2; i<=90; i++) {
             fib[i] = fib[i-1] + fib[i-2];
+            count++;
+            if(i == n) {
+                sb.append(fib[i]).append(" ");
+                sb.append(count-1);
+            }
         }
-
-        System.out.println(fib[n]);
+        System.out.println(sb);
     }
 }
