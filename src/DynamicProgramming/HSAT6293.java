05fc1b62db50a1dbb0107751b6920923031af2e7
gyuhyuk <bruno3@naver.com>
2025-02-03 22:32:09 +0900

    HSAT6293 : 징검다리

---
+++ src/DynamicProgramming/HSAT6293.java
@@ -0,0 +1,44 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class HSAT6293 {
+    static int[] arr;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        dp = new int[n];
+
+        Arrays.fill(dp, 1);
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<i; j++) {
+                if(arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int result = 0;
+        for(int i=0; i<n; i++) {
+            result = Math.max(dp[i], result);
+        }
+
+        System.out.println(result);
+    }
+}
