703e4c611c514ce269189a1e4af9026328184757
gyuhyuk <bruno3@naver.com>
2025-02-10 19:36:47 +0900

    BOJ1402 : 아무래도이문제는A번난이도인것같다

---
+++ src/Math/BOJ1402.java
@@ -0,0 +1,19 @@
+package Math;
+
+import java.util.Scanner;
+
+public class BOJ1402 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            sb.append("yes").append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
