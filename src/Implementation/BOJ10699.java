4d108d11b802fcacd3428819bcfed7472678e98f
gyuhyuk <bruno3@naver.com>
2025-04-12 17:11:15 +0900

    BOJ10699 : 오늘 날짜

---
+++ src/Implementation/BOJ10699.java
@@ -0,0 +1,11 @@
+package Implementation;
+
+import java.time.LocalDate;
+
+public class BOJ10699 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+
+        System.out.println(now);
+    }
+}
